762138fa3ff59becb65d813efe29d8aba32a4eed
paulopeng <522535342@qq.com>
2012-12-10 18:34:23 -0800

    test for add&commit dir

---
+++ test_branch/test2.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test2.c
+ *
+ *    Description:  test for mkdir
+ *
+ *        Version:  1.0
+ *        Created:  12/10/2012 06:32:34 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  paulo (mn), 522535342@qq.com
+ *        Company:  nebutown
+ *
+ * =====================================================================================
+ */
+
+////////////////test for add&commit mkdir
